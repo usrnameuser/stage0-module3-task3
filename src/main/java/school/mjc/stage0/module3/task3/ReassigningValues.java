51f46d84a3bd49ffd5dbd2ff88002fb2903c935d
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-01 13:48:42 +0300

    Update ReassigningValues.java

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first + "\n" + second + "\n" + third);
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+        int first = 15;
+        int second = 6;
+        int third = 4;
+        System.out.println(first + "\n" + second + "\n" + third + "\n" + linkToFirst + "\n" + linkToSecond + "\n" + linkToThird);
     }
 }
